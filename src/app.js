3ba566a3869b40c8566d6b00fd2377f5468ac862
Ritun Chakraborty <mithynead@gmail.com>
2024-10-04 02:47:42 +0530

    6th-change-commite

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-        origin: ["https://hospital-patient-9u1e.vercel.app"],
+        origin: ["https://hospital-patient.vercel.app"],
         method: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
 }));
