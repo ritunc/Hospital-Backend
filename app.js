7648f088cc2e44863f57bc4672f2b7a41e9c1217
Ritun Chakraborty <mithynead@gmail.com>
2024-10-03 20:25:11 +0530

    version2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const { connectionMongodb } = require('./conection/connect')
 const { userLoginrestriction } = require("./middlewareAuth/authmiddleware");
 const fileupload = require('express-fileupload');
