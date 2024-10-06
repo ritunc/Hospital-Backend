b8b5741fb8820725c635af7037da804b9ccb0cbe
Ritun Chakraborty <mithynead@gmail.com>
2024-10-06 11:20:51 +0530

    42th-commite

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-        origin: ["https://hospital-patient-9jyy.vercel.app"],
+        origin: ["https://hospital-patient.vercel.app"],
         method: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
 }));
