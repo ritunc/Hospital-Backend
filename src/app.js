e69658fb466ffeca0431a23377cdd8f48c12e204
Ritun Chakraborty <mithynead@gmail.com>
2025-01-10 23:52:19 +0530

    cors-origin-url-fixed

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-        origin: [`${URL}`],
+        origin: [`https://hospital-patientvercel.vercel.app`],
         method: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
 }));
